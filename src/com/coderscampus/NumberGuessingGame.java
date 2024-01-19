ca7c574a95d0d3a175c6a91393bf52a394a71fa6
deividas strole <deiwis@yahoo.com>
2024-01-19 14:50:04 -0800

    changing code logic

--- src/com/coderscampus/NumberGuessingGame.java
+++ src/com/coderscampus/NumberGuessingGame.java
@@ -7,7 +7,7 @@ public class NumberGuessingGame {
 
 	public static void main(String[] args) {
 
-		Boolean FINISHED = false;
+		//Boolean FINISHED = false;
 		int chances = 5;
 
 		Scanner scanner = new Scanner(System.in);
@@ -15,28 +15,57 @@ public class NumberGuessingGame {
 		int theRandomNumber = random.nextInt(100) + 1;
 		System.out.println("Pick a number between 1 and 100: ");
 
-		while (FINISHED == false) {
+		// while (FINISHED == false) {
 
+		// 	int userInput = Integer.parseInt(scanner.next());
+
+		// 	if (userInput < 1 || userInput > 100) {
+		// 		System.out.println("Your guess is not between 1 and 100, please try again.");
+		// 	} else if (userInput == theRandomNumber) {
+		// 		System.out.println("You win!");
+		// 		FINISHED = true;
+		// 	} else if (chances == 1) {
+		// 		FINISHED = true;
+		// 		System.out.println("You lose, the number to guess was: " + theRandomNumber);
+		// 	} else if (userInput < theRandomNumber) {
+		// 		System.out.println("Please pick a higher number:");
+		// 		chances--;
+		// 	} else {
+		// 		System.out.println("Please pick a lower number:");
+		// 		chances--;
+		// 	}
+		// }
+
+		while (chances > 0) {
+			System.out.println("Pick a number between 1 and 100: ");
 			int userInput = Integer.parseInt(scanner.next());
 
 			if (userInput < 1 || userInput > 100) {
 				System.out.println("Your guess is not between 1 and 100, please try again.");
-			} else if (userInput == theRandomNumber) {
+				continue;
+			}
+
+			chances--;
+
+			if (userInput == theRandomNumber) {
 				System.out.println("You win!");
-				FINISHED = true;
-			} else if (chances == 1) {
-				FINISHED = true;
-				System.out.println("You lose, the number to guess was: " + theRandomNumber);
-			} else if (userInput < theRandomNumber) {
-				System.out.println("Please pick a higher number:");
-				chances--;
+				break;
 			} else {
-				System.out.println("Please pick a lower number:");
-				chances--;
+				if (chances == 0) {
+					System.out.println("You lose, the number to guess was: " + theRandomNumber);
+				} else if (userInput < theRandomNumber) {
+					System.out.println("Please pick a higher number:");
+				} else {
+					System.out.println("Please pick a lower number:");
+				}
+
 			}
 		}
 
-		scanner.close();
+		scanner.close();	
+
 	}
-	
+
+		
 }
+
