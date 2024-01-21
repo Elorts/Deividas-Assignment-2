1629450d96afaaa65f7d464ddab42be52e10e050
deividas strole <deiwis@yahoo.com>
2024-01-21 14:52:57 -0800

    code improvements using continue statement

--- src/com/coderscampus/NumberGuessingGame.java
+++ src/com/coderscampus/NumberGuessingGame.java
@@ -14,26 +14,7 @@ public class NumberGuessingGame {
 		int theRandomNumber = random.nextInt(100) + 1;
 		System.out.println("Pick a number between 1 and 100: ");
 
-		// while (FINISHED == false) {
 
-		// 	int userInput = Integer.parseInt(scanner.next());
-
-		// 	if (userInput < 1 || userInput > 100) {
-		// 		System.out.println("Your guess is not between 1 and 100, please try again.");
-		// 	} else if (userInput == theRandomNumber) {
-		// 		System.out.println("You win!");
-		// 		FINISHED = true;
-		// 	} else if (chances == 1) {
-		// 		FINISHED = true;
-		// 		System.out.println("You lose, the number to guess was: " + theRandomNumber);
-		// 	} else if (userInput < theRandomNumber) {
-		// 		System.out.println("Please pick a higher number:");
-		// 		chances--;
-		// 	} else {
-		// 		System.out.println("Please pick a lower number:");
-		// 		chances--;
-		// 	}
-		// }
 
 		while (chances > 0) {
 			System.out.println("Pick a number between 1 and 100: ");
